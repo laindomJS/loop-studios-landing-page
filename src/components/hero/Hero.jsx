5c9b90c364d339bce9ee0617005a8e2bc9070592
Adrian Rangel <adrijar21@gmail.com>
2022-07-13 19:34:55 -0400

    hero height fixed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,11 +7,11 @@ import bg from '../../assets/desktop/image-hero.jpg';
 
 export const Hero = () => {
   return (
-    <Box w="100%" bgImage={bg} bgSize="cover" bgRepeat="no-repeat" p="4rem" as={motion.header} initial={ {opacity:0} } whileInView={ {opacity:1} } viewport={ {once: true} }>
+    <Box w="100%" h="100%" bgImage={bg} bgSize="cover" bgRepeat="no-repeat" p="4rem" as={motion.header} initial={ {opacity:0} } whileInView={ {opacity:1} } viewport={ {once: true} }>
       <Navbar />
 
      <Box mt="6rem" ml="2rem" w="650px" border="2px solid white" p="2rem">
-      <Heading fontSize="70px" color="white" textTransform="uppercase" fontWeight="thin" letterSpacing="2px">
+      <Heading fontSize="70px" color="white" textTransform="uppercase" fontWeight="normal" letterSpacing="2px">
           Immersive experiences that deliver
         </Heading>
      </Box>
