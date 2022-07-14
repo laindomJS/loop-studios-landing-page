41fbc546f595beff7cf1f9676a9a37684005a327
Adrian Rangel <adrijar21@gmail.com>
2022-07-14 16:25:12 -0400

    added overlay element && corrected width

--- src/components/grid/GridSections.jsx
+++ src/components/grid/GridSections.jsx
@@ -1,38 +1,46 @@
-import { Grid, GridItem, Image } from '@chakra-ui/react';
+import { Grid, GridItem, Image, Box, Text } from '@chakra-ui/react';
 
 export const GridSections = () => {
 
   const srcs = [
     {
     id: 1,
+    text: 'Deep earth',
     dir: '/src/assets/desktop/image-deep-earth.jpg'
   },
   {
     id: 2,
+    text: 'Night Arcade',
     dir: '/src/assets/desktop/image-night-arcade.jpg'
   },
   {
     id: 3,
+    text: 'Soccer team vr',
     dir: '/src/assets/desktop/image-soccer-team.jpg'
   },
   {
     id: 4,
+    text: 'The grid',
     dir: '/src/assets/desktop/image-grid.jpg'
   },
   {
     id: 5,
+    text: 'From up above vr',
     dir: '/src/assets/desktop/image-from-above.jpg'
   },
   {
     id: 6,
+    text: 'Pocket Borealis',
     dir: '/src/assets/desktop/image-pocket-borealis.jpg'
   },
   {
     id: 7,
+    text: 'The curiosity',
     dir: '/src/assets/desktop/image-curiosity.jpg'
   },
   {
     id: 8,
+    text: 'Make it fisheye',
     dir: '/src/assets/desktop/image-fisheye.jpg'
   },
 ]
@@ -41,8 +49,11 @@ export const GridSections = () => {
   return (
     <Grid templateColumns='repeat(4, 1fr)' gap="20px" as="section">
       {srcs.map((src) => (
-        <GridItem key={src.id} as="article">
+        <GridItem key={src.id} as="article" w="fit-content" position="relative" _hover={ {color:'black', cursor:'pointer'} }>
           <Image src={src.dir} />
+          <Box position="absolute" top="20rem" p="1rem" textAlign="left" fontSize="35px" fontFamily="heading" color="white" fontWeight="normal" textTransform="uppercase" letterSpacing="3px">
+            <Text>{src.text}</Text>
+          </Box>
         </GridItem>
       ))}
     </Grid>
--- src/components/product/ProductSection.jsx
+++ src/components/product/ProductSection.jsx
@@ -3,12 +3,18 @@ import { Box, Image, Heading, Text } from '@chakra-ui/react';
 import interactive from '../../assets/desktop/image-interactive.jpg';
 
 export const ProductSection = () => {
+
+  const data = {
+    heading: 'The Leader in interactive VR',
+    text: 'Founded in 2011, Loopstudios has been producing world-class virtual reality project for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.'
+  }
+
   return (
     <Box as="section" p="8rem" position="relative">
       <Image src={interactive} />
-      <Box w="480px" bgColor="white" position="absolute" top="340px" left="670px" p="2rem" initial={ {x:1000} } whileInView={ {x:0} } viewport={ {once:true} }>
-        <Heading mb="1rem" fontSize="40px" letterSpacing="2px" fontWeight="normal" textTransform="uppercase">The Leader in interactive VR</Heading>
-        <Text fontSize="15px">Founded in 2011, Loopstudios has been producing world-class virtual reality project for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</Text>
+      <Box w="490px" bgColor="white" position="absolute" top="340px" left="670px" p="2rem">
+        <Heading mb="1rem" fontSize="40px" letterSpacing="2px" fontWeight="normal" textTransform="uppercase">{data.heading}</Heading>
+        <Text fontSize="15px">{data.text}</Text>
       </Box>
     </Box>
   )
